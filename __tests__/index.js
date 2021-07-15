75179a1890f2fc7b123178eca5e57619b1550ff8
MiroslavKolomiets <kolomiets.miroslav@pdffiller.team>
2021-07-15 22:29:39 +0300

    Fix line break in test

--- __tests__/index.js
+++ __tests__/index.js
@@ -7,8 +7,7 @@ const result =
   '- proxy: 123.234.53.22\n' +
   '- timeout: 50\n' +
   '+ timeout: 20\n' +
-  '+ verbose: true\n';
-
+  '+ verbose: true';
 
 test('Compare files.', () => {
   expect(compareFiles('test-data/file1.json', 'test-data/file2.json')).toBe(result);
