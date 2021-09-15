37bb0b6824514745986823321cbbb79c989b29c4
MiroslavKolomiets <kolomiets.miroslav@pdffiller.team>
2021-09-15 18:42:35 +0300

    Add inner func to plain formatter.

--- src/formatter/plain.js
+++ src/formatter/plain.js
@@ -12,24 +12,27 @@ const renderChild = (value) => {
 
 const getPath = (step) => (step ? step.concat('.') : '');
 
-const plain = (ast, step) => {
+const plain = (ast) => {
   if (!ast) {
     return null;
   }
+  const inner = (innerAst, innerStep) => {
+    const path = getPath(innerStep);
 
-  const path = getPath(step);
+    const log = {
+      nested: (item) => `${inner(item.children, `${innerStep ? `${innerStep}.${item.key}` : item.key}`)}`,
+      deleted: (item) => `Property '${path}${item.key}' was removed`,
+      added: (item) => `Property '${path}${item.key}' was added with value: ${renderChild(item.value)}`,
+      changed: (item) => `Property '${path}${item.key}' was updated. From ${renderChild(item.oldValue)} to ${renderChild(item.newValue)}`,
+      'not-modified': () => undefined,
+    };
 
-  const log = {
-    nested: (item) => `${plain(item.children, `${step ? `${step}.${item.key}` : item.key}`)}`,
-    deleted: (item) => `Property '${path}${item.key}' was removed`,
-    added: (item) => `Property '${path}${item.key}' was added with value: ${renderChild(item.value)}`,
-    changed: (item) => `Property '${path}${item.key}' was updated. From ${renderChild(item.oldValue)} to ${renderChild(item.newValue)}`,
-    'not-modified': () => undefined,
+    const result = innerAst.map((item) => log[item.type](item));
+
+    return result.filter((item) => item !== undefined).join('\n');
   };
 
-  const result = ast.map((item) => log[item.type](item));
-
-  return result.filter((item) => item !== undefined).join('\n');
+  return inner(ast)
 };
 
 export default plain;
