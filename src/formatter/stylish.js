5353d7b65136fba0c66bf97b74e3c48f2d37867f
MiroslavKolomiets <kolomiets.miroslav@pdffiller.team>
2021-09-15 14:51:44 +0300

    Fix indents in stylish formatter.

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 const indentStep = 1;
 
-const getIndent = (level) => '  '.repeat(level * indentStep);
+const getIndent = (level) => '    '.repeat(level * indentStep);
 
 const stringify = (data, level) => {
   const inner = (innerData, innerLevel) => {
@@ -23,21 +23,22 @@ const stylish = (ast) => {
   if (!ast) {
     return null;
   }
-  const inner = (innerAst, level = 1) => {
+  const inner = (innerAst, level) => {
     const indent = getIndent(level);
+    console.log(indent.length);
 
     const log = {
-      nested: (item) => `${indent} ${item.key}: ${inner(item.children, level + 1)}`,
-      deleted: (item) => `${indent} - ${item.key}: ${stringify(item.value, level + 1)}`,
-      added: (item) => `${indent} + ${item.key}: ${stringify(item.value, level + 1)}`,
-      changed: (item) => `${indent} - ${item.key}: ${stringify(item.oldValue, level + 1)}\n${indent} + ${item.key}: ${stringify(item.newValue, level + 1)}`,
-      'not-modified': (item) => `${indent}   ${item.key}: ${stringify(item.value, level + 1)}`,
+      nested: (item) => `${indent}${item.key}: ${inner(item.children, level + 1)}`,
+      deleted: (item) => `${indent}- ${item.key}: ${stringify(item.value, level)}`,
+      added: (item) => `${indent}+ ${item.key}: ${stringify(item.value, level)}`,
+      changed: (item) => `${indent}- ${item.key}: ${stringify(item.oldValue, level)}\n${indent}+ ${item.key}: ${stringify(item.newValue, level)}`,
+      'not-modified': (item) => `${indent}  ${item.key}: ${stringify(item.value, level)}`,
     };
 
-    return `{\n${innerAst.map((item) => log[item.type](item)).join('\n')}\n${indent}}`;
+    return `{\n${innerAst.map((item) => log[item.type](item)).join('\n')}\n${getIndent(level - 1)}}`;
   };
 
-  return inner(ast, 0);
+  return inner(ast, 1);
 };
 
 export default stylish;
